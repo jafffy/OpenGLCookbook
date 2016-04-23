bd87c24e987c3efbd887215609cc42b8fa9749d6
Jaewon Choi <jainersoer@ajou.ac.kr>
2016-04-24 02:23:56 +0900

    Render triangle

--- src/triangle.cpp
+++ src/triangle.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <cassert>
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
@@ -42,14 +43,55 @@ int main(void)
 
 	glClearColor(0.0f, 0.0f, 0.3f, 0.0f);
 
+	GLuint vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	assert(vao != -1);
+
+	GLuint program = LoadShaders("triangle.vert", "triangle.frag");
+	assert(program != -1);
+
+
+	static const GLfloat g_vertex_buffer_data[] = {
+		-1.0f, -1.0f, 0.0f,
+		 1.0f, -1.0f, 0.0f,
+		 0.0f,  1.0f, 0.0f,
+	};
+
+	GLuint vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
+	assert(vbo != -1);
+
+	glBindVertexArray(0);
+
 	while (!glfwWindowShouldClose(window))
 	{
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glfwSwapBuffers(window);
+		glUseProgram(program);
 
+		glBindVertexArray(vao);
+
+		glEnableVertexAttribArray(0);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glDisableVertexAttribArray(0);
+
+		glBindVertexArray(0);
+
+		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+
+	glDeleteBuffers(1, &vbo);
+	glDeleteVertexArrays(1, &vao);
+	glDeleteProgram(program);
 
 	glfwTerminate();
 
