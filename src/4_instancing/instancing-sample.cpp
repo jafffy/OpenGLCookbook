5fb6cfe58b7dada408c3138089f1a220c3654e8d
Jaewon Choi <jainersoer@ajou.ac.kr>
2016-04-30 02:35:15 +0900

    Fix clipping issue

--- src/4_instancing/instancing-sample.cpp
+++ src/4_instancing/instancing-sample.cpp
@@ -24,62 +24,57 @@ public:
 		glClearColor(0.0f, 0.0f, 0.3f, 0.0f);
 
 		glGenVertexArrays(1, &_vao);
+		assert(_vao != -1);
+
 		glBindVertexArray(_vao);
-		
-		if (_vao == -1)
-			return false;
-
-		_program = LoadShaders("instancing.vert", "instancing.frag");
-		assert(_program != -1);
-
-		static const GLfloat g_vertex_buffer_data[] = {
-			-1.0f, -1.0f, 0.0f,
-			 1.0f, -1.0f, 0.0f,
-			 0.0f,  1.0f, 0.0f,
-		};
-
-		glGenBuffers(1, &_vbo);
-		glBindBuffer(GL_ARRAY_BUFFER, _vbo);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
-
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
-
-		assert(_vbo != -1);
-
-		GLfloat* transform_buffer_data = new float[16 * 4];
 		{
-			for (int i = 0; i < 4; ++i) {
-				auto T = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, (float)i, 0.0f));
-				auto R = glm::rotate(glm::mat4(1.0f), _globalTimer, glm::vec3(0.0f, 1.0f, 0.0f));
-				auto S = glm::scale(glm::mat4(1.0f), glm::vec3(1.0f, 1.0f, 1.0f));
+			_program = LoadShaders("instancing.vert", "instancing.frag");
+			assert(_program != -1);
 
-				auto M = T * R * S;
+			static const GLfloat g_vertex_buffer_data[] = {
+				-1.0f, -1.0f, 0.0f,
+				 1.0f, -1.0f, 0.0f,
+				 0.0f,  1.0f, 0.0f,
+			};
 
-				memcpy(transform_buffer_data + 16 * i, glm::value_ptr(M), sizeof(float) * 16);
+			glGenBuffers(1, &_vbo);
+			assert(_vbo != -1);
+
+			glBindBuffer(GL_ARRAY_BUFFER, _vbo);
+			glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
+			GLfloat* transform_buffer_data = new float[16 * 4];
+			{
+				for (int i = 0; i < 4; ++i) {
+					auto T = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, (float)i, 0.0f));
+					auto R = glm::rotate(glm::mat4(1.0f), _globalTimer, glm::vec3(0.0f, 1.0f, 0.0f));
+					auto S = glm::scale(glm::mat4(1.0f), glm::vec3(1.0f, 1.0f, 1.0f));
+
+					auto M = T * R * S;
+
+					memcpy(transform_buffer_data + 16 * i, glm::value_ptr(M), sizeof(float) * 16);
+				}
+
+				glGenBuffers(1, &_transformBufferObject);
+				glBindBuffer(GL_TEXTURE_BUFFER, _transformBufferObject);
+				glBufferData(GL_TEXTURE_BUFFER, sizeof(float) * 16 * 4,
+						transform_buffer_data, GL_STATIC_DRAW);
+
+				glGenTextures(1, &_transformTBO);
+				glBindTexture(GL_TEXTURE_BUFFER, _transformTBO);
+				glTexBuffer(GL_TEXTURE_BUFFER, GL_RGBA32F, _transformBufferObject);
 			}
-
-			glGenBuffers(1, &_transformBufferObject);
-			glBindBuffer(GL_TEXTURE_BUFFER, _transformBufferObject);
-			glBufferData(GL_TEXTURE_BUFFER, sizeof(float) * 16 * 4,
-					transform_buffer_data, GL_STATIC_DRAW);
-
-			glGenTextures(1, &_transformTBO);
-			glBindTexture(GL_TEXTURE_BUFFER, _transformTBO);
-			glTexBuffer(GL_TEXTURE_BUFFER, GL_RGBA32F, _transformBufferObject);
+			if (transform_buffer_data) {
+				delete[] transform_buffer_data;
+				transform_buffer_data = nullptr;
+			}
 		}
-		if (transform_buffer_data) {
-			delete[] transform_buffer_data;
-			transform_buffer_data = nullptr;
-		}
-
 		glBindVertexArray(0);
-
-		glUseProgram(_program);
 
 		_VPID = glGetUniformLocation(_program, "VP");
 		_MsID = glGetUniformLocation(_program, "Ms");
-
-		glUseProgram(0);
 
 		_globalTimer = 0.0f;
 
@@ -99,14 +94,8 @@ public:
 
 	virtual void update(float dt)
 	{
-		auto T = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f));
-		auto R = glm::rotate(glm::mat4(1.0f), _globalTimer, glm::vec3(0.0f, 1.0f, 0.0f));
-		auto S = glm::scale(glm::mat4(1.0f), glm::vec3(1.0f, 1.0f, 1.0f));
-
-		auto M = T * R * S;
-
 		auto V = glm::lookAt(
-				glm::vec3(4,3,3),
+				glm::vec3(3,4,10),
 				glm::vec3(0,0,0),
 				glm::vec3(0,1,0)
 				);
@@ -126,20 +115,21 @@ public:
 
 	virtual void render()
 	{
-		glViewport(0, 0, windowWidth(), windowHeight());
+		glViewport(windowWidth() / 2, windowHeight() / 2, windowWidth(), windowHeight());
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glUseProgram(_program);
 
 		glBindVertexArray(_vao);
+		{
+			glEnableVertexAttribArray(0);
 
-		glEnableVertexAttribArray(0);
+			glActiveTexture(GL_TEXTURE0);
+			glBindTexture(GL_TEXTURE_BUFFER, _transformTBO);
+			glDrawArraysInstanced(GL_TRIANGLES, 0, 3, 4);
 
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_BUFFER, _transformTBO);
-		glDrawArraysInstanced(GL_TRIANGLES, 0, 3, 4);
-
-		glDisableVertexAttribArray(0);
+			glDisableVertexAttribArray(0);
+		}
 
 		glUseProgram(0);
 	}
