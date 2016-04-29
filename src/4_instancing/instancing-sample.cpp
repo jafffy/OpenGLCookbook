d42bec32a098891abc00de4677de31cdf468dc9f
Jaewon Choi <jainersoer@ajou.ac.kr>
2016-04-30 02:15:56 +0900

    Success on instancing but some clipped bug

--- src/4_instancing/instancing-sample.cpp
+++ src/4_instancing/instancing-sample.cpp
@@ -4,6 +4,8 @@
 #include <cstdlib>
 #include <cassert>
 
+#include <vector>
+
 #include <signal.h>
 
 #include <GL/glew.h>
@@ -44,11 +46,38 @@ public:
 
 		assert(_vbo != -1);
 
+		GLfloat* transform_buffer_data = new float[16 * 4];
+		{
+			for (int i = 0; i < 4; ++i) {
+				auto T = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, (float)i, 0.0f));
+				auto R = glm::rotate(glm::mat4(1.0f), _globalTimer, glm::vec3(0.0f, 1.0f, 0.0f));
+				auto S = glm::scale(glm::mat4(1.0f), glm::vec3(1.0f, 1.0f, 1.0f));
+
+				auto M = T * R * S;
+
+				memcpy(transform_buffer_data + 16 * i, glm::value_ptr(M), sizeof(float) * 16);
+			}
+
+			glGenBuffers(1, &_transformBufferObject);
+			glBindBuffer(GL_TEXTURE_BUFFER, _transformBufferObject);
+			glBufferData(GL_TEXTURE_BUFFER, sizeof(float) * 16 * 4,
+					transform_buffer_data, GL_STATIC_DRAW);
+
+			glGenTextures(1, &_transformTBO);
+			glBindTexture(GL_TEXTURE_BUFFER, _transformTBO);
+			glTexBuffer(GL_TEXTURE_BUFFER, GL_RGBA32F, _transformBufferObject);
+		}
+		if (transform_buffer_data) {
+			delete[] transform_buffer_data;
+			transform_buffer_data = nullptr;
+		}
+
 		glBindVertexArray(0);
 
 		glUseProgram(_program);
 
 		_VPID = glGetUniformLocation(_program, "VP");
+		_MsID = glGetUniformLocation(_program, "Ms");
 
 		glUseProgram(0);
 
@@ -61,6 +90,9 @@ public:
 	{
 		glDeleteVertexArrays(1, &_vao);
 		glDeleteBuffers(1, &_vbo);
+		glDeleteBuffers(1, &_transformBufferObject);
+
+		glDeleteTextures(1, &_transformTBO);
 
 		glDeleteProgram(_program);
 	}
@@ -85,6 +117,7 @@ public:
 		auto VP = P * V;
 
 		glUseProgram(_program);
+		glUniform1i(_MsID, 0);
 		glUniformMatrix4fv(_VPID, 1, false, glm::value_ptr(VP));
 		glUseProgram(0);
 
@@ -102,7 +135,9 @@ public:
 
 		glEnableVertexAttribArray(0);
 
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_BUFFER, _transformTBO);
+		glDrawArraysInstanced(GL_TRIANGLES, 0, 3, 4);
 
 		glDisableVertexAttribArray(0);
 
@@ -111,9 +146,12 @@ public:
 
 private:
 	GLuint _vao, _vbo;
+	GLuint _transformBufferObject;
+	GLuint _transformTBO;
 
 	GLuint _program;
 	GLuint _VPID;
+	GLuint _MsID;
 
 	float _globalTimer;
 };
